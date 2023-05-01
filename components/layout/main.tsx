c3744b6a1a21ce651e31f079b94bdecef98f9a34
Nguyen Minh Vu(VuNMy) <vunmy@techvn.mti.co.jp>
2023-05-01 16:31:29 +0700

    Setup Page Layout

--- components/layout/main.tsx
+++ components/layout/main.tsx
@@ -1,15 +1,24 @@
 import { LayoutProps } from '@/models'
+import { Box, Stack } from '@mui/material'
 import Link from 'next/link'
 import React, { useEffect } from 'react'
+import { Footer, Header } from '../common'
 
 export function MainLayout({ children }: LayoutProps) {
 	return (
-		<div>
-			<h1>MAIN Layout</h1>
+		<Stack minHeight='100vh'>
+			<Header />
+
 			<Link href='/'>Home</Link>
-			<Link href='/about'>About</Link>
+			<Link href='/blog'>Blog</Link>
+			<Link href='/works'>Works</Link>
 			<hr style={{ margin: '16px' }} />
-			<div>{children}</div>
-		</div>
+			<div></div>
+			<Box component='main' flexGrow={1}>
+				{children}
+			</Box>
+
+			<Footer />
+		</Stack>
 	)
 }
